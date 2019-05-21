9ec6ab7255b8169140bae9160751579046bd04c3
rebeca-mar <50455311+rebeca-mar@users.noreply.github.com>
2019-05-21 11:06:14 -0500

    Update environment.dev.ts
    Using default file

--- archivos/src/environments/environment.dev.ts
+++ archivos/src/environments/environment.dev.ts
@@ -7,12 +7,11 @@ export const environment = {
   production: false,
   backend: 'http://localhost:8080/',
   firebase: {
-    apiKey: "AIzaSyD2sTYB3aJ0KbZDG_d9XmHpXKmZosh44wk",
-    authDomain: "admin-login-548f4.firebaseapp.com",
-    databaseURL: "https://admin-login-548f4.firebaseio.com",
-    projectId: "admin-login-548f4",
-    storageBucket: "admin-login-548f4.appspot.com",
-    messagingSenderId: "401110938777",
-    appId: "1:401110938777:web:8c6aede859f1db28"
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
   }
 };
